35786a7569199fea2c68018f086c68d2a551d957
wjmgreen <wjmgreen16@gmail.com>
2022-01-28 17:38:26 -0600

    save

--- model/model.py
+++ model/model.py
@@ -1,5 +1,17 @@
 from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtWidgets import QTreeWidgetItem, QTreeWidget
 
 
 class Model(QObject):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.tree = QTreeWidget()
+        self.system = QTreeWidgetItem()
+        self.tree.takeTopLevelItem(self.system)
+
+
+tree = QTreeWidget()
+
+
+print(tree)
+
